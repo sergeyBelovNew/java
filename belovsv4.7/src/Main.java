7827434d913d13ed170e28f25e770515c5ab5504
sergeyBelovNew <115898057+sergeyBelovNew@users.noreply.github.com>
2022-10-21 11:56:48 +0300

    Not working conversion Object[] to Integer[]

--- belovsv4.7/src/Main.java
+++ belovsv4.7/src/Main.java
@@ -1,51 +1,82 @@
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class Main {
+    static final Scanner scannerStreamInt = new Scanner(System.in);
+    static final Scanner scannerStreamStr = new Scanner(System.in);
+
     public static void main(String[] args) {
         //7.1 Калькулятор
+        System.out.println("7.1 Калькулятор");
         executeTask1();
         //7.2 Массив изменяемой длинны
+        System.out.println("7.2 Массив изменяемой длинны");
         executeTask2();
+        scannerStreamInt.close();
+        //Это неверно(повторное закрытие 1 потока in) или при инициализации каждого сканера мы создаем параллельный поток ввода?
+        scannerStreamStr.close();
     }
 
     public static void executeTask1() {
         int choose;
-        Scanner scannerInt = new Scanner(System.in);
-        firstNumber = scannerInt.nextInt();
-        secondNumber = scannerInt.nextInt();
+        System.out.println("Введите 1-ое число:");
+        firstNumber = scannerStreamInt.nextInt();
+        System.out.println("Введите 2-ое число:");
+        secondNumber = scannerStreamInt.nextInt();
         showMenu();
-        choose = scannerInt.nextInt();
+        choose = scannerStreamInt.nextInt();
         runCalculator(choose);
-        scannerInt.close();
     }
 
     public static void executeTask2() {
-        Scanner scannerInt = new Scanner(System.in);
         int[] array = new int[10];
-        if (fillArray().length <= 10)
-            array = Arrays.copyOfRange(fillArray(), 0, fillArray().length - 1);
+        int[] newArray = fillArray();
+        if (newArray.length <= 10)
+            array = Arrays.copyOfRange(newArray, 0, newArray.length);
         else
-            array = Arrays.copyOf(fillArray(), fillArray().length);
-        Arrays.toString(array);
-        scannerInt.close();
-
+            array = Arrays.copyOf(newArray, newArray.length);
+        System.out.println(Arrays.toString(array));
     }
 
     public static int[] fillArray() {
-        Scanner scannerInt = new Scanner(System.in);
-        Scanner scannerStr = new Scanner(System.in);
-        int[] arrayOut;
+        System.out.println("Введите максимальный размер массива:");
+        int maxSize = scannerStreamInt.nextInt();
+        int[] arrayOut = new int[maxSize];
         String breakCycle = "";
-        while (breakCycle != "End") {
-            arrayOut[] = scannerInt.nextInt();
-            breakCycle = scannerStr.nextLine();
+        for (int i = 0; i < maxSize; i++) {
+            arrayOut[i] = scannerStreamInt.nextInt();
+            System.out.println("Прервать ввод(введите End, иначе любой символ)");
+            breakCycle = scannerStreamStr.nextLine();
+            if (breakCycle.equals("End"))
+                return arrayOut;
         }
-        scannerInt.close();
-        scannerStr.close();
         return arrayOut;
     }
+/* Не работает конвертация Object[] в Integer[]
+    public static int[] fillArrayMap() {
+        HashMap<Integer, String> hashMap = new HashMap<>();
+        String breakEnd = "";
+        while (!breakEnd.equals("End")) {
+            System.out.println("Введите значение массива: ");
+            System.out.println("Прервать ввод(введите End, иначе любой символ)");
+            hashMap.put(scannerStreamInt.nextInt(), breakEnd = scannerStreamStr.nextLine());
+        }
+        Object[] tempObj = Arrays.copyOf(hashMap.values().toArray(), hashMap.size());
+        //Ошибка
+        Integer[]tempInteger = Arrays.stream(tempObj).toArray(Integer[]:: new);
+        return convertIntegerToInt(tempInteger);
+    }
 
+    //Есть ли метод для конвертации?
+    public static int[] convertIntegerToInt(Integer[] source) {
+        int[] out = new int[source.length];
+        for (int i = 0; i < source.length; i++)
+            out[i] = source[i];
+        return out;
+    }
+*/
     public static void runCalculator(int chooseOption) {
         Scanner scannerInt = new Scanner(System.in);
         while (chooseOption != 6) {
@@ -53,53 +84,59 @@ public class Main {
                 case 1:
                     System.out.println("Сумма: ");
                     add(firstNumber, secondNumber);
+                    break;
                 case 2:
-                    System.out.println("Произведение: ");
+                    System.out.println("Разность: ");
                     subtract(firstNumber, secondNumber);
+                    break;
                 case 3:
                     System.out.println("Произведение: ");
                     multiply(firstNumber, secondNumber);
+                    break;
                 case 4:
-                    System.out.println("Разность: ");
+                    System.out.println("Деление: ");
                     divide(firstNumber, secondNumber);
+                    break;
                 case 5:
                     System.out.println("Возведение в степень: ");
                     exponentiation(firstNumber, secondNumber);
+                    break;
                 default:
                     break;
             }
-            chooseOption = scannerInt.nextInt();
-            scannerInt.close();
+            if (chooseOption != 6)
+                chooseOption = scannerInt.nextInt();
         }
     }
 
     public static void add(int num1, int num2) {
-        System.out.print(num1 + num2);
+        System.out.println(num1 + num2);
     }
 
 
     public static void subtract(int num1, int num2) {
-        System.out.print(num1 - num2);
+        System.out.println(num1 - num2);
     }
-//
+
+    //
     public static void multiply(int num1, int num2) {
-        System.out.print(num1 * num2);
+        System.out.println(num1 * num2);
     }
 
     public static void divide(int num1, int num2) {
         if (num2 != 0)
-            System.out.print(num1 / num2);
-        else System.out.print("Деление на ноль");
+            System.out.println(num1 / num2);
+        else System.out.println("Деление на ноль");
     }
 
     public static void exponentiation(int num1, int degree) {
         if (degree >= 0)
-            System.out.print(Math.pow(num1, degree));
-        else System.out.print("Значение степени должно быть положительным");
+            System.out.println(Math.pow(num1, degree));
+        else System.out.println("Значение степени должно быть положительным");
     }
 
     public static void showMenu() {
-        System.out.println("Чтобы выбрать операцию введите число:\n1 - сумма\n2 - разность\n3 - произведение\n4 - деленеи\n5 - возведение в степень");
+        System.out.println("Чтобы выбрать операцию введите число:\n1 - сумма\n2 - разность\n3 - произведение\n4 - деленеи\n5 - возведение в степень\n6 - выход");
     }
 
     private static int firstNumber, secondNumber;
